f157a1479281c43642a67519797de31623b289ce
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-11 11:48:26 -0500

    Implemented Operator Precedence

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log(40 + 8 + 23 - 10);
 console.log("Jonas");
 console.log(23);
 
-let firstName = "Matilda";
+// let firstName = "Matilda";
 
 console.log(firstName);
 console.log(firstName);
@@ -66,3 +66,59 @@ job = "teacher"
 
 lastName = "Schmedtmann";
 console.log(lastName);
+
+// ======================================================
+// Basic Operators
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); 
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; 
+// 15
+
+x += 10;
+// x = x + 10 = 25
+
+x *= 4; 
+// x = x * 4 = 100
+
+x++; 
+// x = x + 1
+
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+// ======================================================
+// Operator Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
