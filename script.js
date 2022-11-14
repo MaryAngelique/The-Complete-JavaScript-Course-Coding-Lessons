ee3bf650d224d1b0b21b23f07d07799df9ba6046
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-14 10:47:10 -0500

    Implemented Bonus solution

--- script.js
+++ script.js
@@ -342,6 +342,7 @@ TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 GOOD LUCK 😀
 */
 
+/*
 const scoreDolphins = (96 + 108 + 89) / 3;
 const scoreKoalas = (88 + 91 + 110) / 3;
 console.log(scoreDolphins, scoreKoalas);
@@ -352,4 +353,20 @@ if (scoreDolphins > scoreKoalas) {
   console.log('Koalas win the trophy 🏆');
 } else if (scoreDolphins === scoreKoalas) {
   console.log('Both win the trophy!');
+}
+*/
+
+// BONUS 1
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log('Dolphins win the trophy 🏆');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log('Koalas win the trophy 🏆');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+  console.log('Both win the trophy!');
+} else {
+  console.log('No one wins the trophy 😭');
 }
