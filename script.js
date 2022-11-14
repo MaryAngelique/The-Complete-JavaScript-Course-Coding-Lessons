f100d93b95cbc115d470fdae4d0b8bacdf9e6d7d
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-14 12:04:19 -0500

    Implemented Statements and Expressions

--- script.js
+++ script.js
@@ -412,3 +412,16 @@ if (day === "Monday") {
 } else {
   console.log("Not a valid day!");
 }
+
+// ======================================================
+// Statements and Expressions
+3 + 4
+1991
+true && false && !false
+
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+
+const me = "Jonas";
+console.log(`I'm ${2037 - 1991} years old ${me}`);
