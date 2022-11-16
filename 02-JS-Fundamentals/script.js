dd4f95d3de68d782d7e9f92fdfd8995eef6bece2
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-15 23:44:43 -0500

    Implemented CalcAge and yearsUntilRetirement functions

--- 02-JS-Fundamentals/script.js
+++ 02-JS-Fundamentals/script.js
@@ -82,3 +82,25 @@ function fruitProcessor(apples, oranges) {
   return juice;
 }
 console.log(fruitProcessor(2, 3));
+
+// ======================================================
+// Reviewing Functions
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+  const age = calcAge(birthYeah);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
