d02285c8dd8e26fa4bc448514f890bff6a969a64
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-16 12:31:46 -0500

    Implemented Introduction to Objects

--- 02-JS-Fundamentals/script.js
+++ 02-JS-Fundamentals/script.js
@@ -258,3 +258,21 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
+
+// ======================================================
+// Introduction to Objects
+const jonasArray = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"]
+];
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"]
+};
