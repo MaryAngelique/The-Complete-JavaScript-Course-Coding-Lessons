ac69eaa57b9053ebaa05d3f5226260943ccdcdf2
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-17 13:29:40 -0500

    Implemented while loop to return the dice

--- 02-JS-Fundamentals/script.js
+++ 02-JS-Fundamentals/script.js
@@ -494,3 +494,23 @@ for (let exercise = 1; exercise < 4; exercise++) {
     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
   }
 }
+
+// ======================================================
+// The while Loop
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loop is about to end...');
+}
