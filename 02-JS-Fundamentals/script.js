449c1b9bce0b73fa1000999d1cab7af8684f8f25
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-17 13:12:58 -0500

    Implemented Looping Backwards and Loops in Loops

--- 02-JS-Fundamentals/script.js
+++ 02-JS-Fundamentals/script.js
@@ -419,6 +419,58 @@ for (let rep = 1; rep <= 30; rep++) {
 
 // ======================================================
 // Looping Arrays, Breaking and Continuing
+// const jonas = [
+//   "Jonas",
+//   "Schmedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+//   true
+// ];
+// const types = [];
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+// jonas[5] does NOT exist
+
+// for (let i = 0; i < jonas.length; i++) {
+//   // Reading from jonas array
+//   console.log(jonas[i], typeof jonas[i]);
+
+//   // Filling types array
+//   // types[i] = typeof jonas[i];
+//   types.push(typeof jonas[i]);
+// }
+
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+// console.log(ages);
+
+// // continue and break
+// console.log("--- ONLY STRINGS ---")
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] !== "string") continue;
+
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+// console.log("--- BREAK WITH NUMBER ---")
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] === "number") break;
+
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+// ======================================================
+// Looping Backwards and Loops in Loops
 const jonas = [
   "Jonas",
   "Schmedtmann",
@@ -427,44 +479,18 @@ const jonas = [
   ["Michael", "Peter", "Steven"],
   true
 ];
-const types = [];
 
-// console.log(jonas[0])
-// console.log(jonas[1])
-// ...
-// console.log(jonas[4])
-// jonas[5] does NOT exist
+// 0, 1, ..., 4
+// 4, 3, ..., 0
 
-for (let i = 0; i < jonas.length; i++) {
-  // Reading from jonas array
-  console.log(jonas[i], typeof jonas[i]);
-
-  // Filling types array
-  // types[i] = typeof jonas[i];
-  types.push(typeof jonas[i]);
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
 }
 
-console.log(types);
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
 
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
-
-for (let i = 0; i < years.length; i++) {
-  ages.push(2037 - years[i]);
-}
-console.log(ages);
-
-// continue and break
-console.log("--- ONLY STRINGS ---")
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] !== "string") continue;
-
-  console.log(jonas[i], typeof jonas[i]);
-}
-
-console.log("--- BREAK WITH NUMBER ---")
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] === "number") break;
-
-  console.log(jonas[i], typeof jonas[i]);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+  }
 }
