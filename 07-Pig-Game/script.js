ef5667d5df5a908237f42efc13dedf7ed2eed06d
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-20 00:57:23 -0500

    Implementd selecting HTML elements in script.js using querySelector

---
+++ 07-Pig-Game/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentScore, activePlayer, playing;
