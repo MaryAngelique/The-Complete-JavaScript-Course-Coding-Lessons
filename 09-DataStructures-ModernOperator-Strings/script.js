e00de9171c2c56614acdf212f37a268677d384e9
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-23 23:17:31 -0500

    Implemented Challenge MVP -- mapping through a log of events

--- 09-DataStructures-ModernOperator-Strings/script.js
+++ 09-DataStructures-ModernOperator-Strings/script.js
@@ -1,266 +1,266 @@
 "use strict";
 
-const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [weekdays[5]]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
+// const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekdays[5]]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
 
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
+// const restaurant = {
+//   name: "Classico Italiano",
+//   location: "Via Angelo Tavanti 23, Firenze, Italy",
+//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  // ES6 enhanced object literals
-  openingHours,
+//   // ES6 enhanced object literals
+//   openingHours,
 
-  order(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+//   order(starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
 
-  orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
+//   orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
+//     console.log(
+//       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+//     );
+//   },
 
-  orderPasta(ing1, ing2, ing3) {
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
-  },
+//   orderPasta(ing1, ing2, ing3) {
+//     console.log(
+//       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+//     );
+//   },
 
-  orderPizza(mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
-};
+//   orderPizza(mainIngredient, ...otherIngredients) {
+//     console.log(mainIngredient);
+//     console.log(otherIngredients);
+//   },
+// };
 
-// ======================================================
-// String Methods Practice
+// // ======================================================
+// // String Methods Practice
 
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+// const flights =
+//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-// 🔴 Delayed Departure from FAO to TXL (11h25)
-//              Arrival from BRU to FAO (11h45)
-//   🔴 Delayed Arrival from HEL to FAO (12h05)
-//            Departure from FAO to LIS (12h30)
+// // 🔴 Delayed Departure from FAO to TXL (11h25)
+// //              Arrival from BRU to FAO (11h45)
+// //   🔴 Delayed Arrival from HEL to FAO (12h05)
+// //            Departure from FAO to LIS (12h30)
 
-const getCode = str => str.slice(0, 3).toUpperCase();
+// const getCode = str => str.slice(0, 3).toUpperCase();
 
-for (const flight of flights.split("+")) {
-  const [type, from, to, time] = flight.split(";");
-  const output = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll(
-    "_",
-    " "
-  )} ${getCode(from)} ${getCode(to)} (${time.replace(":", "h")})`.padStart(36);
-  console.log(output);
-}
+// for (const flight of flights.split("+")) {
+//   const [type, from, to, time] = flight.split(";");
+//   const output = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll(
+//     "_",
+//     " "
+//   )} ${getCode(from)} ${getCode(to)} (${time.replace(":", "h")})`.padStart(36);
+//   console.log(output);
+// }
 
-// ======================================================
-// Coding Challenge #4
+// // ======================================================
+// // Coding Challenge #4
 
-/* 
-Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+// /* 
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
 
-The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+// The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
 
-THIS TEST DATA (pasted to textarea)
-underscore_case
- first_name
-Some_Variable 
-  calculate_AGE
-delayed_departure
+// THIS TEST DATA (pasted to textarea)
+// underscore_case
+//  first_name
+// Some_Variable 
+//   calculate_AGE
+// delayed_departure
 
-SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
-underscoreCase      ✅
-firstName           ✅✅
-someVariable        ✅✅✅
-calculateAge        ✅✅✅✅
-delayedDeparture    ✅✅✅✅✅
+// SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+// underscoreCase      ✅
+// firstName           ✅✅
+// someVariable        ✅✅✅
+// calculateAge        ✅✅✅✅
+// delayedDeparture    ✅✅✅✅✅
 
-HINT 1: Remember which character defines a new line in the textarea 😉
-HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
-HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
-HINT 4: This challenge is difficult on purpose, so start watching the solution in case you"re stuck. Then pause and continue!
+// HINT 1: Remember which character defines a new line in the textarea 😉
+// HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+// HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+// HINT 4: This challenge is difficult on purpose, so start watching the solution in case you"re stuck. Then pause and continue!
 
-Afterwards, test with your own test data!
+// Afterwards, test with your own test data!
 
-GOOD LUCK 😀
-*/
+// GOOD LUCK 😀
+// */
 
-document.body.append(document.createElement("textarea"));
-document.body.append(document.createElement("button"));
+// document.body.append(document.createElement("textarea"));
+// document.body.append(document.createElement("button"));
 
-document.querySelector("button").addEventListener("click", function () {
-  const text = document.querySelector("textarea").value;
-  const rows = text.split("\n");
+// document.querySelector("button").addEventListener("click", function () {
+//   const text = document.querySelector("textarea").value;
+//   const rows = text.split("\n");
 
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split("_");
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split("_");
 
-    const output = `${first}${second.replace(
-      second[0],
-      second[0].toUpperCase()
-    )}`;
-    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
-  }
-});
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+//     console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+//   }
+// });
 
-// ======================================================
-// Working With Strings - Part 2
+// // ======================================================
+// // Working With Strings - Part 2
 
-// Split and join
-console.log("a+very+nice+string".split("+"));
-console.log("Jonas Schmedtmann".split(" "));
+// // Split and join
+// console.log("a+very+nice+string".split("+"));
+// console.log("Jonas Schmedtmann".split(" "));
 
-const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
+// const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
 
-const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
-console.log(newName);
+// const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+// console.log(newName);
 
-const capitalizeName = function (name) {
-  const names = name.split(" ");
-  const namesUpper = [];
+// const capitalizeName = function (name) {
+//   const names = name.split(" ");
+//   const namesUpper = [];
 
-  for (const n of names) {
-    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
-  }
-  console.log(namesUpper.join(" "));
-};
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(" "));
+// };
 
-capitalizeName("jessica ann smith davis");
-capitalizeName("jonas schmedtmann");
+// capitalizeName("jessica ann smith davis");
+// capitalizeName("jonas schmedtmann");
 
-// Padding
-const message = "Go to gate 23!";
-console.log(message.padStart(20, "+").padEnd(30, "+"));
-console.log("Jonas".padStart(20, "+").padEnd(30, "+"));
+// // Padding
+// const message = "Go to gate 23!";
+// console.log(message.padStart(20, "+").padEnd(30, "+"));
+// console.log("Jonas".padStart(20, "+").padEnd(30, "+"));
 
-const maskCreditCard = function (number) {
-  const str = number + "";
-  const last = str.slice(-4);
-  return last.padStart(str.length, "*");
-};
+// const maskCreditCard = function (number) {
+//   const str = number + "";
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, "*");
+// };
 
-console.log(maskCreditCard(64637836));
-console.log(maskCreditCard(43378463864647384));
-console.log(maskCreditCard("334859493847755774747"));
+// console.log(maskCreditCard(64637836));
+// console.log(maskCreditCard(43378463864647384));
+// console.log(maskCreditCard("334859493847755774747"));
 
-// Repeat
-const message2 = "Bad waether... All Departues Delayed... ";
-console.log(message2.repeat(5));
+// // Repeat
+// const message2 = "Bad waether... All Departues Delayed... ";
+// console.log(message2.repeat(5));
 
-const planesInLine = function (n) {
-  console.log(`There are ${n} planes in line ${"🛩".repeat(n)}`);
-};
-planesInLine(5);
-planesInLine(3);
-planesInLine(12);
+// const planesInLine = function (n) {
+//   console.log(`There are ${n} planes in line ${"🛩".repeat(n)}`);
+// };
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
 
-// ======================================================
-// Working With Strings - Part 2
+// // ======================================================
+// // Working With Strings - Part 2
 
-const airline = "TAP Air Portugal";
-console.log(airline.toLowerCase());
-console.log(airline.toUpperCase());
+// const airline = "TAP Air Portugal";
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
 
-// Fix capitalization in name
-const passenger = "jOnAS"; // Jonas
-const passengerLower = passenger.toLowerCase();
-const passengerCorrect =
-  passengerLower[0].toUpperCase() + passengerLower.slice(1);
-console.log(passengerCorrect);
+// // Fix capitalization in name
+// const passenger = "jOnAS"; // Jonas
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect =
+//   passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
 
-// Comparing emails
-const email = "hello@jonas.io";
-const loginEmail = "  Hello@Jonas.Io \n";
-// const lowerEmail = loginEmail.toLowerCase();
-// const trimmedEmail = lowerEmail.trim();
-const normalizedEmail = loginEmail.toLowerCase().trim();
-console.log(normalizedEmail);
-console.log(email === normalizedEmail);
+// // Comparing emails
+// const email = "hello@jonas.io";
+// const loginEmail = "  Hello@Jonas.Io \n";
+// // const lowerEmail = loginEmail.toLowerCase();
+// // const trimmedEmail = lowerEmail.trim();
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
+// console.log(email === normalizedEmail);
 
-// replacing
-const priceGB = "288,97£";
-const priceUS = priceGB.replace("£", "$").replace(",", ".");
-console.log(priceUS);
-const announcement =
-  "All passengers come to boarding door 23. Boarding door 23!";
-console.log(announcement.replace("door", "gate"));
-// console.log(announcement.replaceAll("door", "gate"));
-console.log(announcement.replace(/door/g, "gate"));
+// // replacing
+// const priceGB = "288,97£";
+// const priceUS = priceGB.replace("£", "$").replace(",", ".");
+// console.log(priceUS);
+// const announcement =
+//   "All passengers come to boarding door 23. Boarding door 23!";
+// console.log(announcement.replace("door", "gate"));
+// // console.log(announcement.replaceAll("door", "gate"));
+// console.log(announcement.replace(/door/g, "gate"));
 
-// Booleans
-const plane = "Airbus A320neo";
-console.log(plane.includes("A320"));
-console.log(plane.includes("Boeing"));
-console.log(plane.startsWith("Airb"));
-if (plane.startsWith("Airbus") && plane.endsWith("neo")) {
-  console.log("Part of the NEW ARirbus family");
-}
+// // Booleans
+// const plane = "Airbus A320neo";
+// console.log(plane.includes("A320"));
+// console.log(plane.includes("Boeing"));
+// console.log(plane.startsWith("Airb"));
+// if (plane.startsWith("Airbus") && plane.endsWith("neo")) {
+//   console.log("Part of the NEW ARirbus family");
+// }
 
-// Practice exercise
-const checkBaggage = function (items) {
-  const baggage = items.toLowerCase();
-  if (baggage.includes("knife") || baggage.includes("gun")) {
-    console.log("You are NOT allowed on board");
-  } else {
-    console.log("Welcome aboard!");
-  }
-};
-checkBaggage("I have a laptop, some Food and a pocket Knife");
-checkBaggage("Socks and camera");
-checkBaggage("Got some snacks and a gun for protection");
+// // Practice exercise
+// const checkBaggage = function (items) {
+//   const baggage = items.toLowerCase();
+//   if (baggage.includes("knife") || baggage.includes("gun")) {
+//     console.log("You are NOT allowed on board");
+//   } else {
+//     console.log("Welcome aboard!");
+//   }
+// };
+// checkBaggage("I have a laptop, some Food and a pocket Knife");
+// checkBaggage("Socks and camera");
+// checkBaggage("Got some snacks and a gun for protection");
 
-// ======================================================
-// Working With Strings - Part 1
+// // ======================================================
+// // Working With Strings - Part 1
 
-const airline = "TAP Air Portugal";
-const plane = "A320";
-console.log(plane[0]);
-console.log(plane[1]);
-console.log(plane[2]);
-console.log("B737"[0]);
-console.log(airline.length);
-console.log("B737".length);
-console.log(airline.indexOf("r"));
-console.log(airline.lastIndexOf("r"));
-console.log(airline.indexOf("portugal"));
-console.log(airline.slice(4));
-console.log(airline.slice(4, 7));
-console.log(airline.slice(0, airline.indexOf(" ")));
-console.log(airline.slice(airline.lastIndexOf(" ") + 1));
-console.log(airline.slice(-2));
-console.log(airline.slice(1, -1));
+// const airline = "TAP Air Portugal";
+// const plane = "A320";
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log("B737"[0]);
+// console.log(airline.length);
+// console.log("B737".length);
+// console.log(airline.indexOf("r"));
+// console.log(airline.lastIndexOf("r"));
+// console.log(airline.indexOf("portugal"));
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+// console.log(airline.slice(0, airline.indexOf(" ")));
+// console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
 
-const checkMiddleSeat = function (seat) {
-  // B and E are middle seats
-  const s = seat.slice(-1);
-  if (s === "B" || s === "E") console.log("You got the middle seat 😬");
-  else console.log("You got lucky 😎");
-};
+// const checkMiddleSeat = function (seat) {
+//   // B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === "B" || s === "E") console.log("You got the middle seat 😬");
+//   else console.log("You got lucky 😎");
+// };
 
-checkMiddleSeat("11B");
-checkMiddleSeat("23C");
-checkMiddleSeat("3E");
-console.log(new String("jonas"));
-console.log(typeof new String("jonas"));
-console.log(typeof new String("jonas").slice(1));
+// checkMiddleSeat("11B");
+// checkMiddleSeat("23C");
+// checkMiddleSeat("3E");
+// console.log(new String("jonas"));
+// console.log(typeof new String("jonas"));
+// console.log(typeof new String("jonas").slice(1));
 
 // ======================================================
 // Coding Challenge #3
@@ -274,3 +274,38 @@ Let"s continue with our football betting app! This time, we have a map with a lo
       [FIRST HALF] 17: ⚽️ GOAL
 GOOD LUCK 😀
 */
+
+const gameEvents = new Map([
+    [17, "⚽️ GOAL"],
+    [36, "🔁 Substitution"],
+    [47, "⚽️ GOAL"],
+    [61, "🔁 Substitution"],
+    [64, "🔶 Yellow card"],
+    [69, "🔴 Red card"],
+    [70, "🔁 Substitution"],
+    [72, "🔁 Substitution"],
+    [76, "⚽️ GOAL"],
+    [80, "⚽️ GOAL"],
+    [92, "🔶 Yellow card"],
+]);
+  
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+// 2.
+gameEvents.delete(64);
+// 3.
+console.log(
+    `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+    `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+// 4.
+for (const [min, event] of gameEvents) {
+    const half = min <= 45 ? "FIRST" : "SECOND";
+    console.log(`[${half} HALF] ${min}: ${event}`);
+}
