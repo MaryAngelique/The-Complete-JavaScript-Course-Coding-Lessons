97518e6b18027283743cb44e5833741ed7fd30d2
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-23 23:52:37 -0500

    Implemented Map Iteration

--- 09-DataStructures-ModernOperator-Strings/script.js
+++ 09-DataStructures-ModernOperator-Strings/script.js
@@ -309,3 +309,39 @@ for (const [min, event] of gameEvents) {
     const half = min <= 45 ? "FIRST" : "SECOND";
     console.log(`[${half} HALF] ${min}: ${event}`);
 }
+  
+
+// ======================================================
+// Maps: Iteration
+const question = new Map([
+    ["question", "What is the best programming language in the world?"],
+    [1, "C"],
+    [2, "Java"],
+    [3, "JavaScript"],
+    ["correct", 3],
+    [true, "Correct 🎉"],
+    [false, "Try again!"],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+    if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt("Your answer"));
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get("correct") === answer));
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
