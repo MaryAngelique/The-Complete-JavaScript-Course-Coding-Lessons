ae708249048d7fd5859fdc134c5733547f9db15a
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-24 11:10:53 -0500

    Implemented Set() method

--- 09-DataStructures-ModernOperator-Strings/script.js
+++ 09-DataStructures-ModernOperator-Strings/script.js
@@ -377,3 +377,37 @@ rest.set(document.querySelector("h1"), "Heading");
 console.log(rest);
 console.log(rest.size);
 console.log(rest.get(arr));
+
+// ======================================================
+// Sets
+const ordersSet = new Set([
+    "Pasta",
+    "Pizza",
+    "Pizza",
+    "Risotto",
+    "Pasta",
+    "Pizza",
+]);
+
+console.log(ordersSet);
+console.log(new Set("Jonas"));
+console.log(ordersSet.size);
+console.log(ordersSet.has("Pizza"));
+console.log(ordersSet.has("Bread"));
+ordersSet.add("Garlic Bread");
+ordersSet.add("Garlic Bread");
+ordersSet.delete("Risotto");
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+    new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+);
+
+console.log(new Set("jonasschmedtmann").size);
