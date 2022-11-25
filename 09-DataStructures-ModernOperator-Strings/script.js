ccbdcaa0b8effcd2b8f97240d7ae6c21bf97439b
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-25 10:35:26 -0500

    Implemented Looping Objects -- Object keys, values, and entries

--- 09-DataStructures-ModernOperator-Strings/script.js
+++ 09-DataStructures-ModernOperator-Strings/script.js
@@ -493,3 +493,29 @@ const scorers = {};
 for (const player of game.scored) {
     scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
+
+// ======================================================
+// Looping Objects: Object Keys, Values, and Entries
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
