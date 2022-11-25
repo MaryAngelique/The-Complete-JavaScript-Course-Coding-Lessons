925e47bd44c3abb05a0440508b35cde416b21725
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-25 12:52:04 -0500

    Implemented Nullish Coalescing Operator

--- 09-DataStructures-ModernOperator-Strings/script.js
+++ 09-DataStructures-ModernOperator-Strings/script.js
@@ -651,3 +651,13 @@ rest2.owner &&= "<ANONYMOUS>";
 
 console.log(rest1);
 console.log(rest2);
+
+// ======================================================
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or "")
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
