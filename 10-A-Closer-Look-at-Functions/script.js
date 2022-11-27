57f25a2b20c982da55f2128f05d368e8f091590a
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-26 22:33:06 -0500

    Implemented more example of callback functions

--- 10-A-Closer-Look-at-Functions/script.js
+++ 10-A-Closer-Look-at-Functions/script.js
@@ -91,3 +91,22 @@ const high5 = function () {
 };
 document.body.addEventListener("click", high5);
 ["Jonas", "Martha", "Adam"].forEach(high5);
+
+// ======================================================
+// Functions Returning Functions
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet("Hey");
+greeterHey("Jonas");
+greeterHey("Steven");
+
+greet("Hello")("Jonas");
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr("Hi")("Jonas");
