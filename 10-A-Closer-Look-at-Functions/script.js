fd5e04192184a7a152f43662c4c3d58f20fa8a3c
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-27 22:40:31 -0500

    Implemented Immediately Invoked function

--- 10-A-Closer-Look-at-Functions/script.js
+++ 10-A-Closer-Look-at-Functions/script.js
@@ -287,3 +287,27 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
 // [5, 2, 3]
 // [1, 5, 3, 9, 6, 1]
+
+// ======================================================
+// Immediately Invoked Function Expressions (IIFE)
+const runOnce = function () {
+  console.log("This will never run again");
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log("This will never run again");
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log("This will ALSO never run again"))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
