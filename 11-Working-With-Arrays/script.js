553cd77a6538f27e426b7629e5864b910b42e4a0
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-29 22:31:09 -0500

    Implemented challenge instructions

--- 11-Working-With-Arrays/script.js
+++ 11-Working-With-Arrays/script.js
@@ -306,3 +306,21 @@ movements.forEach(function (mov, i, arr) {
         console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
     }
 });
+
+// ======================================================
+// forEach With Maps and Sets
+const currencies = new Map([
+    ["USD", "United States dollar"],
+    ["EUR", "Euro"],
+    ["GBP", "Pound sterling"],
+]);
+
+currencies.forEach(function (value, key, map) {
+    console.log(`${key}: ${value}`);
+});
+
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+    console.log(`${value}: ${value}`);
+});
