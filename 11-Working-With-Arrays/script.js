f7dd0798ff6d82aafa3bbff9257cccc08a078fcf
Angelique Abacajan <ma_utsey@yahoo.com>
2022-12-01 18:35:07 -0500

    Implemented Array Methods -- filter, reduce, map, flatMap, split, and join

--- 11-Working-With-Arrays/script.js
+++ 11-Working-With-Arrays/script.js
@@ -620,3 +620,58 @@ labelBalance.addEventListener("click", function () {
 
     const movementsUI2 = [...document.querySelectorAll(".movements__value")];
 });
+
+// ======================================================
+// Array Methods Practice
+
+const bankDepositSum = accounts
+    .flatMap(acc => acc.movements)
+    .filter(mov => mov > 0)
+    .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum);
+
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000).length;
+
+const numDeposits1000 = accounts
+    .flatMap(acc => acc.movements)
+    .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+
+console.log(numDeposits1000);
+
+let a = 10;
+console.log(++a);
+console.log(a);
+
+const { deposits, withdrawals } = accounts
+    .flatMap(acc => acc.movements)
+    .reduce(
+        (sums, cur) => {
+          // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+          sums[cur > 0 ? "deposits" : "withdrawals"] += cur;
+          return sums;
+        },
+        { deposits: 0, withdrawals: 0 }
+    );
+
+console.log(deposits, withdrawals);
+
+const convertTitleCase = function (title) {
+    const capitzalize = str => str[0].toUpperCase() + str.slice(1);
+
+    const exceptions = ["a", "an", "and", "the", "but", "or", "on", "in", "with"];
+
+    const titleCase = title
+      .toLowerCase()
+      .split(" ")
+      .map(word => (exceptions.includes(word) ? word : capitzalize(word)))
+      .join(" ");
+
+    return capitzalize(titleCase);
+};
+
+console.log(convertTitleCase("this is a nice title"));
+console.log(convertTitleCase("this is a LONG title but not too long"));
+console.log(convertTitleCase("and here is another title with an EXAMPLE"));
