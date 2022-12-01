54fbe83c3d51b1c199767f895f6aab79cfb2c032
Angelique Abacajan <ma_utsey@yahoo.com>
2022-11-30 23:26:06 -0500

    Implemented Sorting Arrays

--- 11-Working-With-Arrays/script.js
+++ 11-Working-With-Arrays/script.js
@@ -557,3 +557,33 @@ const overalBalance2 = accounts
     .flatMap(acc => acc.movements)
     .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2);
+
+// ======================================================
+// Sorting Arrays
+
+// Strings
+const owners = ["Jonas", "Zach", "Adam", "Martha"];
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
